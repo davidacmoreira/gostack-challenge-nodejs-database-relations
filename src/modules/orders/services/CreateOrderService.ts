2f0ade68705dd28f9afb16412b7b48d020c4b229
David Moreira <davidmoreira37@gmail.com>
2020-11-29 10:37:28 +0000

    Add order entities, repository, controller and services

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,76 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const findCustomer = await this.customersRepository.findById(customer_id);
+
+    if (!findCustomer) {
+      throw new AppError('Could not find any customer with the given id');
+    }
+
+    const existentProducts = await this.productsRepository.findAllById(
+      products,
+    );
+
+    // if (!existentProducts.length) {
+    //   throw new AppError('Could not find any products with the given ids');
+    // }
+
+    const existentProductsIds = existentProducts.map(product => product.id);
+
+    const inexistentProducts = products.filter(
+      product => !existentProductsIds.includes(product.id),
+    );
+
+    if (inexistentProducts.length) {
+      throw new AppError('Could not find all the products with the given ids');
+    }
+
+    const productsWithInsufficientQuantity = products.filter(
+      product =>
+        existentProducts.filter(
+          existentProduct => existentProduct.id === product.id,
+        )[0].quantity < product.quantity,
+    );
+
+    if (productsWithInsufficientQuantity.length) {
+      throw new AppError(
+        'Could not find available quantities for all the products',
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existentProducts.filter(
+        existentProduct => existentProduct.id === product.id,
+      )[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: findCustomer,
+      products: serializedProducts,
+    });
+
+    const updateProductsQuantities = order.order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        existentProducts.filter(
+          existentProduct => existentProduct.id === product.product_id,
+        )[0].quantity - product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(updateProductsQuantities);
+
+    return order;
   }
 }
 
